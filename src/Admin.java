fff03c4683e4cd0d38a98f74a1311869b595cb3c
Thasleena <thasleenaa7@gmail.com>
2024-12-27 09:45:43 +0530

    ticketbooking

---
+++ src/Admin.java
@@ -0,0 +1,21 @@
+package src;
+
+public class Admin extends User{
+     public Admin(String name, String email, String phoneno, String password) {
+        super(name, email, phoneno, password);
+    }
+
+    void addMoviList(){
+
+ }
+ void editMovies(){
+
+ }
+   void editTheatres(){
+
+   }
+ void editShowNo(){
+
+ }
+
+}
